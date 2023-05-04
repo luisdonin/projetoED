00f261ab535848c33bf59d0e885e0acc1da14420
J. L. B. Donin <88415069+luisdonin@users.noreply.github.com>
2023-05-04 15:21:34 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#include "headers/heap/FD.h"
+//#include "headers/stack/PD.h"
+
+#define MAX  100
+
+/*
+ *Rules: 
+ *	He can't watch two shows at the same time
+ *	When starting a new show, he downloads the episodes in chronological order
+ *	He watches it in chronological oder as well
+ *	He can only watch the shows he has already downloaded
+ *	if he starts show A, he can start show B, but he must finish show B before going back to A, and he must continue A from the point he had stopped before going to B
+ *	A show is completed when all episodes are completed
+ * */
+
+/*
+ * Input: start S E
+ * Input: download quantity of show 20min
+ *
+ * */
+//scanf("%s" ,S);
+//scanf("%s", E);
+//scanf("%d", &A);
+
+/*
+ *if (input: start show episode --> starting show) (name of the show)
+ *else if(input: download n_episodes len_episode --> n episodes of show downloaded)
+ *else if(input: watch n --> n episodes of show watched (n minutes)
+ *The problem: I need to compare all these string inputs, efficiently :c
+ *
+ *
+ * */
+//strCompare = strcmp(S, S_compare_start);
+
+/*char writeToFile(char *E){
+    FILE *f = fopen("shows.txt", "w");
+    if(f == NULL){
+        printf("Error!");
+        exit(1);
+    }
+    fprintf(f, "Show name: %s", E);
+    fclose(f);
+
+    return 1;
+}*/
+typedef struct Node{
+
+    char *data;
+   struct  Node *next;
+
+}node;
+
+node *insert(node *list, char *E){
+    node *newNode = (node*)malloc(sizeof(node));
+    newNode->data = E;
+    newNode->next = list;
+    return newNode;
+}
+
+char compareStrings(node *list, char *S, char *E){
+    char S_compare_start[]="start", S_compare_download[]="download";
+
+    int A, t;
+    if(strcmp(S, S_compare_start) == 0){
+        scanf("%s %d", E, &A);
+        insert(list, E);
+
+        printf("starting %s %d\n", E, A);
+        //writeToFile(E);
+
+
+    }else
+    if(strcmp(S, S_compare_download) == 0){
+        printf("%s\n", E);
+        scanf("%d %d", &A, &t);//find out why it's not printing the name of the show
+
+        printf("%d episodes of %s downloaded\n", A,E);
+
+    }
+}
+
+
+
+int main(){
+	/*
+	 * S --> instruction "start"
+	 * E --> name of the show
+	 * A --> episode number
+	 * */
+    node *list;
+
+
+	char S[6], E[MAX],  S_compare_exit[] = "f";
+    
+	while(1){
+        scanf("%s", S);
+        if(strcmp(S, S_compare_exit) == 0){
+            return 0;
+        }
+        compareStrings(list,S, E);
+    }
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ make.sh
@@ -0,0 +1,1 @@
+clear && gcc -o main main.c && ./main
